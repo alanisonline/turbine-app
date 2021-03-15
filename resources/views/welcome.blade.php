da94926be87961b7edcd4e0293f2f3fac673d600
alanisonline <alanisonline2012@gmail.com>
2021-03-15 15:49:22 +0000

    Added svg logo and css, and linked within relevant views. This also includes the map-texture image which is updated each time the app runs, as it pulls a static map from the bing api in routes/web.php. That file isn't included in the commit currently to avoid sharing the API key, but will be added with the key removed later.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.layout')
 @section('content')
-<canvas id="c"></canvas>
+<div class="container">
+        <canvas id="c"></canvas>
+        <div class="ui-menu">
+            <div class="ui-menu-header">
+                <span>TOOLS</span>
+            </div>
+            <ul id="ui-navigation" class="ui-navigation">
+                <li class="selected"><div>Turbine 1</div></li>
+            </ul>
+            <div class="ui-menu-footer">
+                <img src="./assets/img/footer-logo.svg" alt="logo">
+            </div>
+        </div>
+    </div>
 @endsection('content')
