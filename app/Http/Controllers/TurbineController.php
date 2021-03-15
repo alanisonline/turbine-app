1bd176ed5147485dd5155f32622309f4818698d5
alanisonline <alanisonline2012@gmail.com>
2021-03-15 16:09:37 +0000

    Set up models and controllers for Site, Turbine and Component

---
+++ app/Http/Controllers/TurbineController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Turbine;
+use Illuminate\Http\Request;
+
+class TurbineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Turbine  $turbine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Turbine $turbine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Turbine  $turbine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Turbine $turbine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Turbine  $turbine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Turbine $turbine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Turbine  $turbine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Turbine $turbine)
+    {
+        //
+    }
+}
